ff6ba945aac8d06887a3905c581d3c2e101fee95
Abdelrahman Elsafty <abdelrhmanelsafty74@gmail.com>
2026-01-22 18:51:55 +0200

    adding __str__ instead of regular method

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,21 +2,21 @@ class Book:
     def __init__(self, title:str , author:str ):
         self.title = title
         self.author = author
-    def get_info(self):
+    def __str__(self):
         return f"Book: {self.title} by {self.author}"
 
 class EBook(Book):
     def __init__(self, title:str , author:str , file_size:float):
         super().__init__(title, author)
         self.file_size = file_size
-    def get_info(self):
+    def __str__(self):
         return f"E-Book: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 class PrintBook(Book):
     def __init__(self, title:str , author:str , page_count:int):
         super().__init__(title, author)
         self.page_count = page_count
-    def get_info(self):
+    def __str__(self):
         return f"Print Book: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 class Library:
@@ -25,5 +25,5 @@ class Library:
         self.books.append(book)
     def list_books(self):
         for book in self.books:
-            print(book.get_info())
+            print(book)
         
